9fc32fc185685aa1243cfdb920050762da7e998a
a1cd <71281043+a1cd@users.noreply.github.com>
2022-01-24 22:33:14 -0500

    Finishing Touches

--- src/main/java/ScrabbleScorer.java
+++ src/main/java/ScrabbleScorer.java
@@ -2,63 +2,69 @@ import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.util.*;
-import java.util.List;
 
+/**
+ * The ScrabbleScorer class is used to calculate the amount of points a string would count
+ * for in the game of SCRABBLE. This class verifies that a word exists in the scrabble
+ * dictionary and then prints the score of the word (if it is valid)
+ *
+ * @author 24wilber
+ * @version 1/24/2022
+ */
+@SuppressWarnings("FieldMayBeFinal")
 public class ScrabbleScorer {
-    public ArrayList<String> Dict;
-    public ArrayList<ArrayList<String>> SuperDict = new ArrayList<>(27);
-    /**
-     * The scores for all of the characters
-     * element 0 is 0 because it makes it more efficient
-     */
+    private ArrayList<String> Dict;
     private static int[] points = {0, 1, 3, 3, 2, 1, 4, 2, 4, 1, 8, 5, 1, 3, 1, 1, 3, 10, 1, 1, 1, 1, 4, 4, 8, 4, 10};
 
+    /**
+     * Creates the ScrabbleScorer object and calls `buildDictionary()`
+     */
     public ScrabbleScorer() {
-        this.Dict = new ArrayList<String>();
-        this.SuperDict = new ArrayList<ArrayList<String>>();
+        Dict = new ArrayList<>();
+        buildDictionary();
     }
 
-    @Override
+
+    /**
+     * takes the dictionary file named SCRABBLE_WORDS.txt and creates a list of all the words in the file.
+     * It then sorts the file
+     */
     public void buildDictionary() {
         try {
-            FileReader reader = new FileReader(new File("src/main/resources/SCRABBLE_WORDS.txt"));
+            //find the file to make things easier
+            //file can be in any of these places
+            File scrabbleWords =new File("./SCRABBLE_WORDS.txt");
+
+            FileReader reader = new FileReader(scrabbleWords);
             Scanner scanner = new Scanner(reader);
             while (scanner.hasNextLine()) {
                 String word = scanner.nextLine();
-                Dict.add(word);
+                Dict.add(word.toUpperCase());//uppercase just in case
             }
-            Arrays.sort(points);
-        } catch (FileNotFoundException e) {
-            e.printStackTrace();
-        }
-    }
-    public void buildBetterDictionary() {
-        byte DecapitalizeAndShift = 0x1F;
-        for (int i = 0; i < 27; i++) {
-            SuperDict.add(new ArrayList<String>());
-        }
-        try {
-            FileReader reader = new FileReader(new File("src/main/resources/SCRABBLE_WORDS.txt"));
-            Scanner scanner = new Scanner(reader);
-            while (scanner.hasNextLine()) {
-                String word = scanner.nextLine();
-                SuperDict.get(word.charAt(0) & DecapitalizeAndShift).add(word);
-            }
-            for (int i = 0; i < SuperDict.size(); i++) {
-                Collections.sort(SuperDict.get(i));
-            }
+            Collections.sort(Dict);
         } catch (FileNotFoundException e) {
             e.printStackTrace();
         }
     }
 
+    /**
+     * does a binary search for the word in the dictionary to see if it is a valid scrabble word.
+     * @param word the word to be tested
+     * @return a boolean; true if valid, false if invalid
+     */
     public boolean isValidWord(String word) {
-        return Collections.binarySearch(Dict, word)>=0;
+        return Collections.binarySearch(Dict, word.toUpperCase())>=0;
     }
-    public boolean isValidWordFast(String word) {
-        byte DecapitalizeAndShift = 0x1F;
-        return Collections.binarySearch(SuperDict.get(word.charAt(0)&DecapitalizeAndShift), word)>=0;
-    }
+
+    /**
+     * this function takes each character in the string word and calulates the character's place in the alphabet.
+     * (a -> 1; b -> 2; etc.) It then gets the points value in the array points at the number. blah blah
+     * @implNote *basically* this turns characters into numbers and asks points how many points for this.
+     * for each character in the string `word`
+     * @implSpec its pretty fast (^◡^)
+     * @param word the word want to know the points for
+     * @return the total scrabble ~imaginary~ points for said word
+     */
     public int getWordScore(String word) {
         byte DecapitalizeAndShift = 0x1F; // in binary: `0001 1111`
         // capital letters look like this: 010x xxxx
@@ -72,30 +78,32 @@ public class ScrabbleScorer {
 
         int sum = 0;
         // parse word one letter at a time
+
         byte currentCharacter;
         for (int i = 0; i < word.length(); i++) { // i use this so i dont allocate a char[] of the contents of word
             sum += points[DecapitalizeAndShift & word.charAt(i)];
         }
-//        for (char i: word.toCharArray()) { // i use this so i dont allocate a char[] of the contents of word
-//            sum += points[DecapitalizeAndShift & i];
+        // find index of each letter
+        // add the corresponding to a sum score
+        return sum;
+    }
+//    public int getWordScoreSlow(String word) {
+//        ArrayList<Character> alphabet = new ArrayList<>(List.of('A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z'));
+//        ArrayList<Integer> slowPoints = new ArrayList<>(List.of(1, 3, 3, 2, 1, 4, 2, 4, 1, 8, 5, 1, 3, 1, 1, 3, 10, 1, 1, 1, 1, 4, 4, 8, 4, 10));
+//        int sum = 0;
+//        for (Character i : word.toCharArray()) {
+//            sum += slowPoints.get(alphabet.indexOf(Character.toUpperCase(i)));
 //        }
-        // find index of each letter
-        // add the corresponding to a sum score
-        return sum;
-    }
-    public int getWordScoreSlow(String word) {
-        ArrayList<Character> alphabet = new ArrayList<>(List.of('A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z'));
-        ArrayList<Integer> slowPoints = new ArrayList<>(List.of(1, 3, 3, 2, 1, 4, 2, 4, 1, 8, 5, 1, 3, 1, 1, 3, 10, 1, 1, 1, 1, 4, 4, 8, 4, 10));
-        int sum = 0;
-        for (Character i : word.toCharArray()) {
-            sum += slowPoints.get(alphabet.indexOf(Character.toUpperCase(i)));
-        }
+//
+//        // find index of each letter
+//        // add the corresponding to a sum score
+//        return sum;
+//    }
 
-        // find index of each letter
-        // add the corresponding to a sum score
-        return sum;
-    }
-
+    /**
+     * This initiates the program for the user to start scoring their scrabble words.
+     * @param args literally nothing, you dont have to put anything in here but you can!
+     */
     public static void main(String[] args) {
         ScrabbleScorer app = new ScrabbleScorer();
         System.out.println("* Welcome to the Scrabble Word Scorer app *");
@@ -103,15 +111,20 @@ public class ScrabbleScorer {
         Scanner userIn = new Scanner(System.in);
         try {
             while (true) {
-                System.out.print("Enter a word to score");
+                System.out.print("Enter a word to score or 0 to quit: ");
                 userWord = userIn.nextLine();
-                if (app.isValidWord(userWord)) System.out.println(app.getWordScore(userWord));
-                else {
-
-                }
+                if (userWord.equals("0"))
+                    break;
+                else if (app.isValidWord(userWord))
+                    System.out.println(userWord+" = "+app.getWordScore(userWord));
+                else
+                    System.out.println(userWord+" is not a valid word in the dictionary");
             }
+            System.out.println("Exiting the program thanks for playing");
+            userIn.close();
         } catch (Exception e) {
             e.printStackTrace();
         }
+
     }
 }
